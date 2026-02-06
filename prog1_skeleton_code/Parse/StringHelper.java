bb7e75dfdf84a9c6d7871c540e1d1dd6266a2b83
clayton <claytonhouser10@gmail.com>
2026-02-05 22:41:51 -0600

    Adding finished strings

--- prog1_skeleton_code/Parse/StringHelper.java
+++ prog1_skeleton_code/Parse/StringHelper.java
@@ -11,6 +11,87 @@ public class StringHelper {
         return -1;// Failed to find a back slash
     }
 
+    private static boolean isOctDigit(char c) {
+        if (c >= '0' && c <= '7') {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isHexDigit(char c) {
+        if (c >= '0' && c <= '7') {
+            boolean isZeroTo=(c >= '0' && c <= '9');
+            boolean isLowerTo=(c >= 'a' && c <= 'f');
+            boolean isUpperTo=(c >= 'A' && c <= 'F');
+
+            return isZeroTo | isLowerTo | isUpperTo;
+        }
+        return false;
+    }
+
+    private static int getOctEnd(String escapeString, int startDigit) {
+        int digiCount = 0;
+        for (int i = startDigit; i < escapeString.length(); i++) {
+            char curChar = escapeString.charAt(i);
+            if (!isOctDigit(curChar)) {
+                return i;
+            }
+            digiCount++;
+            if (digiCount >= 3) return i + 1;
+        }
+        return escapeString.length();
+    }
+
+    private static int getHexEnd(String escapeString, int startDigit) {
+        for (int i = startDigit; i < escapeString.length(); i++) {
+            char curChar = escapeString.charAt(i);
+            if (!isHexDigit(curChar)) {
+                return i;
+            }
+        }
+        return escapeString.length();
+    }
+
+    private static char getNormEscape(char escapeType) {
+        if (escapeType == 'n') {
+            return '\n';
+        } else if (escapeType == 't') {
+            return '\t';
+        } else if (escapeType == 'r') {
+            return '\r';
+        } else if (escapeType == '"') {
+            return '\"';
+        } else if (escapeType == '\\') {
+            return '\\';
+        } else if (escapeType == 'b') {
+            return '\b';
+        } else if (escapeType == 'f') {
+            return '\f';
+        } else if (escapeType == 'v') {
+            return (char) 0x0B;
+        } else if (escapeType == 'a') {
+            return (char) 0x07;
+        } else if (escapeType == '?') {
+            return '?';
+        } else if (escapeType == '\'') {
+            return '\'';
+        } else {
+            return escapeType;
+        }
+    }
+
+    public static char hexToChar(String hexSub) {
+        int intForm = Integer.parseInt(hexSub, 16);
+        char charForm = (char) intForm;
+        return charForm;
+    }
+
+    public static char octToChar(String octSub) {
+        int intForm = Integer.parseInt(octSub, 8);
+        char charForm = (char) intForm;
+        return charForm;
+    }
+
     public static String convertEsc(String text) {
         StringBuilder sb = new StringBuilder(text);
         int lastIndex = 0;
@@ -28,28 +109,25 @@ public class StringHelper {
             // escape char after / is now at the correct index to remove and swap with real
             // escape
             char escapeType = sb.charAt(lastIndex);
-            if (escapeType == 'n') {
-                sb.setCharAt(lastIndex, '\n');
-            } else if (escapeType == 't') {
-                sb.setCharAt(lastIndex, '\t');
-            } else if (escapeType == 'r') {
-                sb.setCharAt(lastIndex, '\r');
-            } else if (escapeType == '"') {
-                sb.setCharAt(lastIndex, '\"');
-            } else if (escapeType == '\\') {
-                sb.setCharAt(lastIndex, '\\');
-            } else if (escapeType == 'b') {
-                sb.setCharAt(lastIndex, '\b');
-            } else if (escapeType == 'f') {
-                sb.setCharAt(lastIndex, '\f');
-            } else if (escapeType == 'v') {
-                sb.setCharAt(lastIndex, (char) 0x0B);
-            } else if (escapeType == 'a') {
-                sb.setCharAt(lastIndex, (char) 0x07);
-            } else if (escapeType == '?') {
-                sb.setCharAt(lastIndex, '?');
-            } else if (escapeType == '\'') {
-                sb.setCharAt(lastIndex, '\'');
+            // handle hex
+            if (escapeType == 'x') {
+                // is hex
+                int hexEnd = getHexEnd(sb.toString(), (lastIndex+1));
+                String hexSub = sb.toString().substring((lastIndex+1), hexEnd);
+                char charForm = hexToChar(hexSub);
+                //set new lastIndex
+                sb.delete(lastIndex+1, hexEnd);
+                sb.setCharAt(lastIndex, charForm);
+            } else if (isOctDigit(escapeType)) {
+                // is oct
+                int octEnd = getOctEnd(sb.toString(), lastIndex);
+                String octSub = sb.toString().substring(lastIndex, octEnd);
+                char charForm = octToChar(octSub);
+                sb.delete(lastIndex+1, octEnd);
+                sb.setCharAt(lastIndex, charForm);
+            } else {
+                // Handle normal escapes
+                sb.setCharAt(lastIndex, getNormEscape(escapeType));// Else normal escape
             }
             lastIndex++;// Increment to next index to avoid infinite loop
         }
